7c56dad85e958189ef923a60a5088a62b32af5a8
Alana <alanabeatriztobias@gmail.com>
2026-06-03 10:10:51 -0300

    Entendendo o conceito de fetch e .then

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ fetch("https://jsonplaceholder.typicode.com/users")
 
 /* 2. Esse .then() 
 - Espera a resposta chegar
-- Que é uma linguagem no JS */
+- Que é uma linguagem no JS
+- E depois resposta.json() 
+- Transforma os dados em objeto JavaScript */
 
-.then((resposta)=> resposta.json())
+.then((resposta) => resposta.json())
+.then((dados) => {
+
+console.log(dados)
+
+})
